2bb16ccbc455d9088e0fd1b99119ffae1aa75845
danieldanielito <113646615+danieldanielito@users.noreply.github.com>
2023-02-02 11:14:41 -0300

    boton copiar

--- javas.js
+++ javas.js
@@ -6,6 +6,11 @@ La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"*/
+function btnCopiar(){
+    const texto = mensaje.value
+    textArea.value = texto
+    mensaje.value = ""
+}
 function btnEncriptar(){
 const textoEncriptado=encriptar(textArea.value)
 mensaje.value=textoEncriptado
